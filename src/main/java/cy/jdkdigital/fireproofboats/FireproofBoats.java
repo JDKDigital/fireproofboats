b355730866a6bec0b8e016c851ee8aafffd24819
Jais Djurhuus-Kempel <jdk@combine.dk>
2023-03-19 11:49:50 +0200

    1.19.4 port

--- src/main/java/cy/jdkdigital/fireproofboats/FireproofBoats.java
+++ src/main/java/cy/jdkdigital/fireproofboats/FireproofBoats.java
@@ -1,13 +1,7 @@
 package cy.jdkdigital.fireproofboats;
 
-import cy.jdkdigital.fireproofboats.mixin.MixinBoatType;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.item.BoatItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Blocks;
@@ -24,7 +18,6 @@ import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 @Mod(FireproofBoats.MODID)
 public class FireproofBoats
 {
@@ -36,10 +29,10 @@ public class FireproofBoats
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FireproofBoats.MODID);
 
-    public static final RegistryObject<Item> CRIMSON_BOAT = ITEMS.register("crimson_boat", () -> new BoatItem(false, CRIMSON_TYPE, (new Item.Properties()).stacksTo(1).fireResistant()));
-    public static final RegistryObject<Item> CRIMSON_CHEST_BOAT = ITEMS.register("crimson_chest_boat", () -> new BoatItem(true, CRIMSON_TYPE, (new Item.Properties()).stacksTo(1).fireResistant()));
-    public static final RegistryObject<Item> WARPED_BOAT = ITEMS.register("warped_boat", () -> new BoatItem(false, WARPED_TYPE, (new Item.Properties()).stacksTo(1).fireResistant()));
-    public static final RegistryObject<Item> WARPED_CHEST_BOAT = ITEMS.register("warped_chest_boat", () -> new BoatItem(true, WARPED_TYPE, (new Item.Properties()).stacksTo(1).fireResistant()));
+    public static final RegistryObject<Item> CRIMSON_BOAT = ITEMS.register("crimson_boat", () -> new BoatItem(false, CRIMSON_TYPE, (new Item.Properties()).stacksTo(1)));
+    public static final RegistryObject<Item> CRIMSON_CHEST_BOAT = ITEMS.register("crimson_chest_boat", () -> new BoatItem(true, CRIMSON_TYPE, (new Item.Properties()).stacksTo(1)));
+    public static final RegistryObject<Item> WARPED_BOAT = ITEMS.register("warped_boat", () -> new BoatItem(false, WARPED_TYPE, (new Item.Properties()).stacksTo(1)));
+    public static final RegistryObject<Item> WARPED_CHEST_BOAT = ITEMS.register("warped_chest_boat", () -> new BoatItem(true, WARPED_TYPE, (new Item.Properties()).stacksTo(1)));
 
     public FireproofBoats() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
